beac84a5ab2892b1078a1f0e00a7cde3185adac5
Michał Sopniewski <michal.sopniewski@gmail.com>
2019-09-19 22:20:19 +0200

    array_copy

--- testing/main.cpp
+++ testing/main.cpp
@@ -1,5 +1,6 @@
 #include "tst_optional.h"
 #include "tst_shared_pointer.h"
+#include "tst_array_copy.h"
 
 #include <gtest/gtest.h>
 
---
+++ testing/tst_array_copy.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <numeric>
+#include <gtest/gtest.h>
+#include "../trivial/array_copy.h"
+
+using namespace trivial;
+
+TEST(array_copy, simple_case){
+  int src[10] = {0,1,2,3,4,5,6,7,8,9};
+  int dst[10] = {0};
+
+  copy_array(src, 10, dst, 10);
+  for (int i=0; i<10; i++){
+      EXPECT_EQ(dst[i], src[i]);
+    }
+}
+
+TEST(array_copy, smaller_dst){
+  int src[10] = {0,1,2,3,4,5,6,7,8,9};
+  int dst[7] = {0};
+
+  copy_array(src, 10, dst, 7);
+  for (int i=0; i<7; i++){
+      EXPECT_EQ(dst[i], src[i]);
+    }
+}
+
+TEST(array_copy, bigger_dst){
+  int src[10] = {0,1,2,3,4,5,6,7,8,9};
+  int dst[19] = {0};
+
+  copy_array(src, 10, dst, 10);
+  for (int i=0; i<10; i++){
+      EXPECT_EQ(dst[i], src[i]);
+    }
+}
+
